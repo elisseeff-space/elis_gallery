3bc451d64312338b6f4819ce9feabfff68390ac4
elisseeff-space <pavel.eliseev@gmail.com>
2023-04-27 21:16:32 +0300

    Update admin.py

--- handlers/admin.py
+++ handlers/admin.py
@@ -26,6 +26,16 @@ async def cm_start(message : types.Message):
         await FSMAdmin.photo.set()
         await message.reply('Pls load a photo')
 
+# Go Out from States
+# @dp.message_handler(state="*", commands='Cancel')
+# @dp.message_handler(Text(equals='Cancel', ignore_case=True), state="*")
+async def cancel_handler(message: types.Message, state: FSMContext):
+    current_state = await state.get_state()
+    if current_state is None:
+        return
+    await state.finish()
+    await message.reply('Ok')
+    
 # Catch the first answer from user and write in Dictionary
 # @dp.message_handler(content_types=['photo'], state=FSMAdmin.photo)
 async def load_photo(message: types.Message, state: FSMContext):
@@ -65,23 +75,13 @@ async def load_price(message: types.Message, state: FSMContext):
 
         await state.finish()
 
-# Go Out from States
-# @dp.message_handler(state="*", commands='Cancel')
-# @dp.message_handler(Text(equals='Cancel', ignore_case=True), state="*")
-async def cancel_handler(message: types.Message, state: FSMContext):
-    current_state = await state.get_state()
-    if current_state is None:
-        return
-    await state.finish()
-    await message.reply('Ok')
-
 # Handlers Registration
 def register_handlers_admin(dp : Dispatcher):
     dp.register_message_handler(cm_start, commands='Load', state=None)
+    dp.register_message_handler(cancel_handler, state="*", commands='Cancel')
+    dp.register_message_handler(cancel_handler, Text(equals='Cancel', ignore_case=True), state="*")
     dp.register_message_handler(load_photo, content_types=['photo'], state=FSMAdmin.photo)
     dp.register_message_handler(load_name, state=FSMAdmin.name)
     dp.register_message_handler(load_description, state=FSMAdmin.description)
     dp.register_message_handler(load_price, state=FSMAdmin.price)
-    dp.register_message_handler(cancel_handler, state="*", commands='Cancel')
-    dp.register_message_handler(cancel_handler, Text(equals='Cancel', ignore_case=True), state="*")
     dp.register_message_handler(make_changes_command, commands=['Moderator'], is_chat_admin=True)
