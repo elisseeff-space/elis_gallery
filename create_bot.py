598431e4bdd3d3e8a465cf25d75341e8751b9fdf
elisseeff-space <pavel.eliseev@gmail.com>
2023-04-29 10:26:07 +0300

    webhook

--- create_bot.py
+++ create_bot.py
@@ -2,6 +2,7 @@ from aiogram import Bot
 from aiogram.dispatcher import Dispatcher
 import json
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from data_base import sqllite_db
 
 file = open('config.json', 'r')
 config = json.load(file)
@@ -10,3 +11,12 @@ storage = MemoryStorage()
 
 bot = Bot(token = config['TelegramBotToken'])
 dp = Dispatcher(bot, storage=storage)
+
+async def on_startup(_):
+    print('Gallery Bot online!')
+    sqllite_db.sql_start()
+    await bot.set_webhook(config['URL_APP'])
+
+async def on_startup(_):
+    print('Gallery Bot off!')
+    await bot.delete_webhook()
